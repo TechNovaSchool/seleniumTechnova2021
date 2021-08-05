e71b5a6c4ce9c6b1c5801cbfac625e8bfe91cef0
andrewnova <tech.easy1@outlook.com>
2021-08-04 19:58:54 -0500

    getText,getAttribute

---
+++ src/test/java/selenium/IsDisplayed.java
@@ -0,0 +1,30 @@
+package selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class IsDisplayed {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+
+        WebElement gmail = driver.findElement(By.partialLinkText("Gmail"));
+
+        if(gmail.isDisplayed()){
+            System.out.println("Test Pass");
+        }
+        else {
+            System.out.println("Test Fail");
+        }
+
+        gmail.click();
+
+
+    }
+}
