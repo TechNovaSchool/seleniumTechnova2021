3b38f425b4cc32f8b8cf1d3efa18413260c1ffd4
andrewnova <tech.easy1@outlook.com>
2021-08-19 19:31:52 -0500

    config

--- src/test/java/testNG/PropertiesPractice.java
+++ src/test/java/testNG/PropertiesPractice.java
@@ -1,11 +1,19 @@
 package testNG;
 
+import Utilities.Config;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class PropertiesPractice {
 
@@ -13,7 +21,7 @@ public class PropertiesPractice {
     public void practice() throws IOException {
 
         // 1. Create a path for connection to out file
-        String path = "test.properties";
+        String path = "Configuration.properties";
 
         //2. We will create Properties object
 
@@ -31,7 +39,26 @@ public class PropertiesPractice {
         System.out.println(properties.getProperty("login"));
         System.out.println(properties.getProperty("mybaseURl"));
 
+    }
+
+    @Test
+    public void testBear() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(Config.getProperty("smartBearUrl"));
+
+        WebElement userName = driver.findElement(By.cssSelector("input[id = 'ctl00_MainContent_username']"));
+        WebElement password = driver.findElement(By.cssSelector("input[id = 'ctl00_MainContent_password']"));
+        WebElement signinButton = driver.findElement(By.cssSelector("input[id = 'ctl00_MainContent_login_button']"));
+
+        userName.sendKeys(Config.getProperty("userName"));
+        password.sendKeys(Config.getProperty("password"));
+        signinButton.click();
+
 
     }
+
 
 }
